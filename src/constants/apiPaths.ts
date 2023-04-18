7e51e442421ad6219e3fcf191b975c87b7e2b493
Artsiom Lipin <artsiom_lipin@epam.com>
2023-04-18 16:06:13 +0300

    implement task 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   products: "https://obuyhmgxv8.execute-api.us-east-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://wmbnxqomyb.execute-api.us-east-1.amazonaws.com/dev",
   bff: "https://obuyhmgxv8.execute-api.us-east-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
